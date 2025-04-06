3940b8471a2d9306a80df0b2017f326bbb208311
Antonsalib <aisalib@mail.lipscomb.edu>
2025-04-05 21:10:20 -0500

    You can now pkay tic tac toe

--- server/index.js
+++ server/index.js
@@ -4,15 +4,54 @@ import company from "./api/json/company.json" with {type: "json"}; // Importing 
 const app = express();
 import cors from "cors"; // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 const CORS = cors();
-app.use(CORS);
+import OpenAI from "openai";
+
 const PORT = 3001;
+app.use(CORS);
 import User from './models/user.js';
 import Player from './models/player.js';
 import { syncModels } from "./models/index.js";
 
+
 syncModels();
 
-app.get("/api/company", (req, res) => {
+app.get("/api/game", async (req, res) => {
+  const OPENAI_API_KEY = "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";
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+  const aiModel = "gpt-3.5-turbo";
+  const boardJson = req.query.board;
+
+  if (boardJson && boardJson.length) {
+
+    const prompt = [];
+    prompt.push('You are an expert tic tac toe player.')
+    prompt.push('You play as O. focus on winning, play extremely well. If you see an oppertunity to win, take it. Play aggressively.')
+    prompt.push('for the json content I provide as input, please give me json output in this same format, indicating your next move.')
+    prompt.push('{board:[[],[],[]]}')
+
+    const messages = [
+      {
+        role: "system",
+        content: prompt.join(' '),
+      },
+      {
+        role: "user",
+        content: boardJson,
+      },
+    ];
+
+    const completion = await openai.chat.completions.create({
+      model: aiModel,
+      messages,
+      response_format:{ "type": "json_object" }
+    });
+
+    const aiResponse = completion.choices[0].message.content;
+    res.json({ aiResponse });
+  }
+});
+
+  app.get("/api/company", (req, res) => {
   return res.json(company);
 });
 
